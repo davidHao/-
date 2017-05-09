636d320bce1844bc8f752bc3dbea900671baba27
Bryce Hao <bryce.hao@ticketmaster.com>
2017-05-09 11:48:21 -0400

    Leet code 1 /561

---
+++ src/算法/ArrayPairSum.java
@@ -0,0 +1,16 @@
+package 算法;
+
+import java.util.Arrays;
+
+public class ArrayPairSum {
+    public int arrayPairSum(int[] nums) {
+       Arrays.sort(nums);
+       int ret = 0 ;
+       for(int i=0; i<nums.length; i++){
+           if(i%2 == 0 ){
+               ret = ret + nums[i];
+           }
+       }
+       return ret;
+    }
+}
---
+++ src/算法/TwoSum.java
@@ -0,0 +1,19 @@
+package 算法;
+
+import java.util.HashMap;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ret = new int[2];
+        HashMap<Integer, Integer> hashMap= new HashMap<>();
+        for( int i= 0; i< nums.length; i++) {
+            if(hashMap.containsKey(target - nums[i])){
+                ret[0]= hashMap.get(target- nums[i]);
+                ret[1] = i;
+                break;
+            }
+           hashMap.put(nums[i],i);
+        }
+        return ret;
+    }
+}
